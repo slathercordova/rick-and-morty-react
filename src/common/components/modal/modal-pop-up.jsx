a1051216e6c5223067a95bb89b43648d2c5b7bc8
Slather <slathercordova@gmail.com>
2026-06-16 19:01:51 -0500

    fix imagenes

--- src/common/components/modal/modal-pop-up.jsx
+++ src/common/components/modal/modal-pop-up.jsx
@@ -1,14 +1,20 @@
 import styles from "./modal-pop-up.module.css";
 import { useState, useEffect } from "react";
+import imagen1 from "./../../../common/images/imagen1.jpg"
+import imagen2 from "./../../../common/images/imagen2.jpg"
+import imagen3 from "./../../../common/images/imagen3.jpg"
+import imagen4 from "./../../../common/images/imagen4.jpg"
+import imagen5 from "./../../../common/images/imagen5.jpg"
+import imagen6 from "./../../../common/images/imagen6.jpg"
 
 export function Modal({ open, onClose, children }) {
   const backgrounds = [
-    "/src/common/images/imagen1.jpg",
-    "/src/common/images/imagen2.jpg",
-    "/src/common/images/imagen3.jpg",
-    "/src/common/images/imagen4.jpg",
-    "/src/common/images/imagen5.jpg",
-    "/src/common/images/imagen6.jpg",
+    imagen1,
+    imagen2,
+    imagen3,
+    imagen4,
+    imagen5,
+    imagen6,
   ];
 
   const [bg, setBg] = useState("");
